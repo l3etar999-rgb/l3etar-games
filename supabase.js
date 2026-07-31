fbc298cbb7c4b3e5359ce758e1fcb3d3db14139b
l3etar999-rgb <l3etar999@gmail.com>
2026-07-30 23:18:27 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://btzcugbxgwalwlcfymmr.supabase.co";
+
+const SUPABASE_KEY = "حط هنا Publishable key ديالك";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
