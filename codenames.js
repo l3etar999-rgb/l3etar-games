a1c1db0a3ab618ac3a251486f95f5a82ca560a21
l3etar999-rgb <l3etar999@gmail.com>
2026-07-30 22:08:59 -0700

    Update codenames.js

--- codenames.js
+++ codenames.js
@@ -3,67 +3,126 @@ const board = document.querySelector(".board");
 document.getElementById("createRoom").addEventListener("click", createRoom);
 document.getElementById("joinRoom").addEventListener("click", joinRoom);
 
-function createRoom() {
+
+function createRoom(){
 
     const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
     let code = "";
 
-    for (let i = 0; i < 5; i++) {
-        code += chars[Math.floor(Math.random() * chars.length)];
+    for(let i = 0; i < 5; i++){
+        code += chars[Math.floor(Math.random()*chars.length)];
     }
 
+
     board.innerHTML = `
-        <h2>🎉 Room Tsawbat</h2>
 
-        <p>Sift had code l s7abek:</p>
+    <h2>🎉 Room Tsawbat</h2>
 
-        <div class="room-code">${code}</div>
+    <p>Sift had code l s7abek:</p>
 
-        <button id="copyBtn">📋 Copy Code</button>
+    <div class="room-code">
+        ${code}
+    </div>
 
-        <br><br>
 
-        <button id="startBtn">🎮 Bda L3ba</button>
+    <button id="copyBtn">
+        📋 Copy Code
+    </button>
+
+
+    <br><br>
+
+
+    <button id="startBtn">
+        🎮 Bda L3ba
+    </button>
+
     `;
 
 
-    document.getElementById("copyBtn").onclick = () => {
+    document.getElementById("copyBtn").onclick = ()=>{
 
         navigator.clipboard.writeText(code);
 
-        alert("Code tcopya: " + code);
+        alert("Code tcopya ✅");
 
     };
 
 
-    document.getElementById("startBtn").onclick = () => {
+    document.getElementById("startBtn").onclick = ()=>{
 
-        board.innerHTML = `
-            <h2>🚧 L3ba mazal katsawb...</h2>
-
-            <p>F lmar7ala jaya ghadi yban Board dyal Code Names.</p>
-        `;
+        startGame();
 
     };
 
 }
 
 
-function joinRoom() {
+
+function joinRoom(){
 
     const code = prompt("Dkhel Code:");
 
-    if (!code) return;
+    if(!code) return;
+
 
     board.innerHTML = `
-        <h2>✅ Rak Dkhelti</h2>
 
-        <p>Room:</p>
+    <h2>✅ Rak Dkhelti</h2>
 
-        <div class="room-code">${code.toUpperCase()}</div>
+    <p>Room:</p>
 
-        <p>Tsna Host ybda l3ba.</p>
+    <div class="room-code">
+        ${code.toUpperCase()}
+    </div>
+
+    <p>Tsna host ybda l3ba 🎮</p>
+
     `;
+
+}
+
+
+
+
+function startGame(){
+
+    let gameWords = getRandomWords(25);
+
+
+    let html = `
+
+    <h2>🎮 Code Names</h2>
+
+    <div class="game-board">
+
+    `;
+
+
+    gameWords.forEach(word=>{
+
+        html += `
+
+        <div class="word-card">
+
+            ${word}
+
+        </div>
+
+        `;
+
+    });
+
+
+
+    html += `
+
+    </div>
+
+    `;
+
+
+    board.innerHTML = html;
 
 }
