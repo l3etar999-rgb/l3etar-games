e9a269a140696eb4abaf07ff44d27518c213304c
l3etar999-rgb <l3etar999@gmail.com>
2026-07-30 23:21:41 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 const SUPABASE_URL = "https://btzcugbxgwalwlcfymmr.supabase.co";
 
-const SUPABASE_KEY = "حط هنا Publishable key ديالك";
+
+const SUPABASE_KEY = "sb_publishable_OTmWjphfusY5-9kotaIJ0A_G6-4ZSWA";
 
 
 const supabaseClient = supabase.createClient(
