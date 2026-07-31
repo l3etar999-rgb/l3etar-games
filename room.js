88e115ee3bbd886f025a1937961be2c926ba8a54
l3etar999-rgb <l3etar999@gmail.com>
2026-07-30 22:59:37 -0700

    Update room.js

--- room.js
+++ room.js
@@ -1,17 +1,70 @@
 function createRoom(){
 
-alert("Sawbti Room ✅");
+const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+
+let code = "";
+
+for(let i = 0; i < 5; i++){
+
+code += chars[Math.floor(Math.random()*chars.length)];
 
 }
 
 
+document.getElementById("roomArea").innerHTML = `
+
+<h2>✅ Room Tsawbat</h2>
+
+<p>Code dyal room:</p>
+
+<div class="room-code">
+${code}
+</div>
+
+<p>Sift had code l s7abek 🔥</p>
+
+`;
+
+}
+
+
+
+function showJoin(){
+
+document.getElementById("roomArea").innerHTML = `
+
+<h2>🔑 Dkhol Room</h2>
+
+<input id="codeInput" placeholder="Dkhel Code">
+
+<br><br>
+
+<button onclick="joinRoom()">
+Dkhol
+</button>
+
+`;
+
+}
+
+
+
 function joinRoom(){
 
-let code = prompt("Dkhel Code:");
+let code = document.getElementById("codeInput").value;
+
 
 if(code){
 
-alert("Dkhelti l Room: " + code);
+document.getElementById("roomArea").innerHTML = `
+
+<h2>✅ Dkhlt l Room</h2>
+
+<div class="room-code">
+${code.toUpperCase()}
+</div>
+
+`;
 
 }
 
